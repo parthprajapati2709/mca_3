a51b3601a29fd37cecbadeb063caefb378a433dc
symca48 <symca48@server1.dom>
2025-07-23 14:22:52 +0530

    index page

---
+++ calc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,ans;
+    printf("enter no:");
+    scanf("%d",&a);
+    printf("enter no:");
+    scanf("%d",&b);
+    printf("ans= %d",ans);
+    
+}
